e2fd9dfe6bd5c43535db51dfd6cede320a2d1f69
JEONG <dogug2865@gmail.com>
2021-01-15 00:03:47 +0900

    Added bus crawling

--- app/bus/urls.py
+++ app/bus/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from bus import views
+
 
 urlpatterns = [
-    path(),
+    path('<str:city>/', views.TimeTableView.as_view()),
 ]
--- app/bus/views.py
+++ app/bus/views.py
@@ -1,4 +1,5 @@
-import re
+from rest_framework import status, views
+from rest_framework.response import Response
 
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
@@ -12,23 +13,18 @@ def won_bus_crawling():
     city_title_soup = bs.select('#content > div > div.bus_area > div > h6')
     bus_time_soup = bs.select('#content > div > div.bus_area > div ')
 
-    city_key = list()
+    city_value = list()
     bus_time_value = list()
 
     for city_tags in city_title_soup:
         city = city_tags.get_text()
-        city_key.append(city)
+        city_value.append(city)
 
     for bus_tags in bus_time_soup:
         bus_time = bus_tags.get_text(',', strip=True).split(',')
         bus_time_value.append(bus_time)
 
-    city_timetable = dict.fromkeys(city_key)
-
-    for i in range(len(city_key)):
-        city_timetable[city_key[i]] = bus_time_value[i][1:]
-
-    return city_timetable
+    return city_value, bus_time_value
 
 
 def inje_bus_crawling():
@@ -39,31 +35,21 @@ def inje_bus_crawling():
     city_title_soup = bs.select('#content > div.bus_area > div > h6')
     bus_time_soup = bs.select('#content > div.bus_area > div')
 
-    city_key = list()
+    city_value = list()
     bus_time_value = list()
 
     for city_tags in city_title_soup:
         city = city_tags.get_text()
-        city_key.append(city)
+        city_value.append(city)
 
     for bus_tags in bus_time_soup:
         bus_time = bus_tags.get_text(',', strip=True).split(',')
         bus_time_value.append(bus_time)
 
-    # for value in bus_time:
-    #     if value in city_value:
-    #         continue
-    #     else:
-    #         bus_time_value.append(value)
-    city_timetable = dict.fromkeys(city_key)
-
-    for i in range(len(city_key)):
-        city_timetable[city_key[i]] = bus_time_value[i][1:]
-
-    return city_timetable
+    return city_value, bus_time_value
 
 
-def hun_bus_crawling():
+def hyun_bus_crawling():
     req = Request("http://www.inje-pti.com/index.php?mp=p3_3_3")
     res = urlopen(req)
     html = res.read()
@@ -71,25 +57,34 @@ def hun_bus_crawling():
     city_title_soup = bs.select('#content > div.bus_area > div > h6')
     bus_time_soup = bs.select('#content > div.bus_area > div ')
 
-    city_key = list()
+    city_value = list()
     bus_time_value = list()
 
     for city_tags in city_title_soup:
         city = city_tags.get_text()
-        city_key.append(city)
+        city_value.append(city)
 
     for bus_tags in bus_time_soup:
         bus_time = bus_tags.get_text(',', strip=True).split(',')
         bus_time_value.append(bus_time)
 
-    city_timetable = dict.fromkeys(city_key)
-
-    for i in range(len(city_key)):
-        city_timetable[city_key[i]] = bus_time_value[i][2:]
-
-    return city_timetable
+    return city_value, bus_time_value
 
 
-print('인제: ', inje_bus_crawling())
-print('원통:', won_bus_crawling())
-print('현리:', hun_bus_crawling())
+class TimeTableView(views.APIView):
+
+    def get(self, request, city):
+        if city == 'inje':
+            city_data, time_data = inje_bus_crawling()
+        elif city == 'won':
+            city_data, time_data = won_bus_crawling()
+        elif city == 'hyun':
+            city_data, time_data = hyun_bus_crawling()
+        if len(city_data) > 0 and len(time_data) > 0:
+            timetable = list()
+            for i in range(len(city_data)):
+                timetable.append({'city': f'{city_data[i]}', 'time': f'{time_data[i][1:]}'})
+
+            return Response(data={'response': timetable}, status=status.HTTP_200_OK)
+
+        return Response(data={'response': "No data"}, status=status.HTTP_204_NO_CONTENT)
